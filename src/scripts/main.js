a4b94128bea4474322ee9b499f1493856f59dcc0
Charles Jackson <cpjackson572@gmail.com>
2019-10-28 11:25:11 -0500

    completed lightening exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -116,11 +116,17 @@ const businesses = [
 outEl.innerHTML = "<h1>Active Businesses</h1>"
 
 businesses.forEach(business => {
+    const zipcodeKey = 'addressZipCode'
   outEl.innerHTML += `
     <h2>${business.companyName}</h2>
     <section>
       ${business.addressFullStreet}
     </section>
+    <section>
+      ${business.addressCity}, ${business['addressStateCode']}, ${business[zipcodeKey]}
+    </section>
   `
   outEl.innerHTML += "<hr/>"
 });
+
+// ^^^Lightning Exercise: Add another section sibling to the current one and use object dot notation to display each company's city. Use square bracket notation to display the state code. Use dynamic square bracket notation to add the zip code.
